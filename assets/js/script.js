049da46154e065bc07462e16e6459cb13d36585f
ecwu <bitlabwzh@gmail.com>
2018-08-19 11:36:30 +0800

    delete useless files

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,12 +95,4 @@ $(document).ready(function () {
 
 });
 
-/*--------------popup------------*/
-$('.open-popup-link').magnificPopup({
-  type:'inline',
-  midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  gallery:{
-    enabled:true
-  }
-});
 
